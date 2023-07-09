bfb9cc7f77804fde76ef73c462a6d08470731052
ryanMantegod <ryan.tuglife@hotmail.com>
2023-07-09 11:58:43 -0300

    Adicionando Aula

---
+++ aulas/aula03/src/app/services/animais.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Animal } from '../models/Animal';
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalService {
+  constructor(private http: HttpClient) {}
+
+  buscarAnimais(): Observable<Animal[]> {
+    return this.http.get<Animal[]>("http://localhost:8050/listar-animais")
+  }
+}
